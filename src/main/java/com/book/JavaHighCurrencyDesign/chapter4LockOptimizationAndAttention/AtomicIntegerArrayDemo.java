62838b7485ff46758e8b7dff530f5e214a50b5d8
niceyuanze <niceyuanze@gmail.com>
2017-05-16 14:01:46 +0800

    backup

---
+++ src/main/java/com/book/JavaHighCurrencyDesign/chapter4LockOptimizationAndAttention/AtomicIntegerArrayDemo.java
@@ -0,0 +1,7 @@
+package com.book.JavaHighCurrencyDesign.chapter4LockOptimizationAndAttention;
+
+/**
+ * Created by niceyuanze on 17-5-14.
+ */
+public class AtomicIntegerArrayDemo {
+}
---
+++ src/main/java/com/book/JavaHighCurrencyDesign/chapter4LockOptimizationAndAttention/vector/LockFreeVector.java
@@ -0,0 +1,7 @@
+package com.book.JavaHighCurrencyDesign.chapter4LockOptimizationAndAttention.vector;
+
+/**
+ * Created by niceyuanze on 17-5-14.
+ */
+public class LockFreeVector {
+}
